09c0a2c9103aa2386f8e862439144d1efcc2f2b4
ibrahimroshdy <ibrahimabdelrahman@aucegypt.edu>
2022-09-04 22:58:37 +0200

    [CU-3d2m6ka] - Setup [ADDED] heroku.yml

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -15,4 +15,4 @@ echo "from django.contrib.auth import get_user_model; User = get_user_model(); p
 echo "Collecting static.."
 echo "yes" | python3 manage.py collectstatic
 
-daphne -b 0.0.0.0 -p 8000  -t 60 --application-close-timeout 60 core.asgi:application
+daphne -b 0.0.0.0 -p $PORT  -t 60 --application-close-timeout 60 core.asgi:application
