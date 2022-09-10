1216c983c71ba9b577d84d8d11937502600dd5b2
ibrahimroshdy <ibrahimabdelrahman@aucegypt.edu>
2022-09-10 23:12:17 +0200

    [CU-3g3uqk6] added timeloop

---
+++ core/tasks.py
@@ -0,0 +1,27 @@
+"""
+Timeloop based scheduled tasks
+"""
+from datetime import timedelta
+
+import timeloop
+from loguru import logger
+
+from apps.internet_speedtester import tasks
+
+tl = timeloop.Timeloop()
+interval_time = timedelta(minutes=15)
+
+
+@tl.job(interval=interval_time)
+def internet_speedtests():
+    """
+    Timeloop job defention that runs every specified time above
+    :return:
+    """
+    logger.info(f'Running internet_speedtest @ interval of: {interval_time}')
+    tasks.process_speedtest()
+
+
+if __name__ == "__main__":
+    while True:
+        tl.start(block=True)
