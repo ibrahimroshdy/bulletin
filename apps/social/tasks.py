2907a012a206cb7bdba50f8ce562421f3fdb6ee9
ibrahimroshdy <ibrahimabdelrahman@aucegypt.edu>
2023-02-01 00:47:00 +0200

    [CU-864duzexe] Removed load() function views.py

--- apps/social/tasks.py
+++ apps/social/tasks.py
@@ -4,13 +4,16 @@ import django
 from celery import shared_task
 from django.utils import timezone
 from loguru import logger
+
 from core import messages
+
 # Setup django to be able to access the settings file
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 django.setup()
 
 from apps.social.models import TweetModel, TweetSystemModel
 from apps.social.utils import AbstractTweepy
+from psycopg2 import OperationalError
 
 
 @shared_task
@@ -21,31 +24,33 @@ def random_auto_tweeter_process():
         and randomly chooses a tweet from the database using a model manager in TweetModel()
         then tweets it and updates the DB
     """
-    system_status = TweetSystemModel.objects.get(pk=1)
+    try:
+        system_status = TweetSystemModel.objects.get(pk=1)
 
-    at_twt = AbstractTweepy()
-    tweet = TweetModel.random_tweet.get_random_tweet()
+        at_twt = AbstractTweepy()
+        tweet = TweetModel.random_tweet.get_random_tweet()
 
-    if tweet is not None:
-        twt_bool, twt_response = at_twt.create_tweet(text=tweet.tweet_text)
+        if tweet is not None:
+            twt_bool, twt_response = at_twt.create_tweet(text=tweet.tweet_text)
 
-        if twt_bool:
-            tweet.is_tweeted = True
-            tweet.tweet_date = timezone.now()
-            tweet.save()
+            if twt_bool:
+                tweet.is_tweeted = True
+                tweet.tweet_date = timezone.now()
+                tweet.save()
 
-            sucess_message = f'Tweet #: {tweet.id} posted'
+                sucess_message = f'Tweet #: {tweet.id} posted'
 
-            logger.success(sucess_message)
-            system_status.set_working(message=sucess_message)
+                logger.success(sucess_message)
+                system_status.set_working(message=sucess_message)
+            else:
+                logger.error(f'{twt_response}')
+                system_status.set_error(message=twt_response)
+
         else:
-            logger.error(f'{twt_response}')
-            system_status.set_error(message=twt_response)
-
-    else:
-        logger.warning(messages.__NO_TWEETS_AVAIABLE_IN_DB)
-        system_status.set_maintenance(message=messages.__NO_TWEETS_AVAIABLE_IN_DB)
-
+            logger.warning(messages.__NO_TWEETS_AVAIABLE_IN_DB)
+            system_status.set_maintenance(message=messages.__NO_TWEETS_AVAIABLE_IN_DB)
+    except OperationalError as OE:
+        logger.error(f'OperationalError DB: {OE}')
 
 # if __name__ == '__main__':
 #     random_auto_tweeter_process()
